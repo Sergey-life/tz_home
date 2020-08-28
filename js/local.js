ab0fd166d056fb0903ef18edabce64d2ab50e57f
Sergey-life <sergei.kharchenko888@gmail.com>
2020-08-28 23:38:17 +0300

    test7

--- js/local.js
+++ js/local.js
@@ -1,49 +1,50 @@
 let inputIn = document.querySelector('.form-control');
 let button = document.querySelector('button');
 
-button.onclick = function () {
-    const box = document.querySelector('#box')
+    button.onclick = function () {
+        const box = document.querySelector('#box')
 
-    if (box.checked)  {
+        if (box.checked)  {
 
-        let value = inputIn.value;
-        let input = value;
-        let items = input.split(", ");
+            let value = inputIn.value;
+            let input = value;
+            let items = input.split(",");
 
-        function permutator(inputArr) {
-            var results = [];
+            function permutator(inputArr) {
+                var results = [];
 
-            function permute(arr, memo) {
-                var cur, memo = memo || [];
+                function permute(arr, memo) {
+                    var cur, memo = memo || [];
 
-                for (var i = 0; i < arr.length; i++) {
-                    cur = arr.splice(i, 1);
-                    if (arr.length === 0) {
-                        results.push(memo.concat(cur));
+                    for (var i = 0; i < arr.length; i++) {
+                        cur = arr.splice(i, 1);
+                        if (arr.length === 0) {
+                            results.push(memo.concat(cur));
+                        }
+                        permute(arr.slice(), memo.concat(cur));
+                        arr.splice(i, 0, cur[0]);
                     }
-                    permute(arr.slice(), memo.concat(cur));
-                    arr.splice(i, 0, cur[0]);
+
+                    return results;
                 }
 
-                return results;
+                return permute(inputArr);
             }
 
-            return permute(inputArr);
+            const div = document.getElementById('container')
+            const list = permutator(items)
+
+            list.forEach(i => {
+                const el = document.createElement('p')
+
+                el.textContent = i.toString()
+
+                div.appendChild(el)
+                console.log(list)
+            })
+
+            return false
         }
-
-        const div = document.getElementById('container')
-        const list = permutator(items)
-
-        list.forEach(i => {
-            const el = document.createElement('p')
-
-            el.textContent = i.toString()
-
-            div.appendChild(el)
-        })
-
-        return false
     }
-}
 
 
