ac5dea713ee9fba68d3ae731da3602119716c358
Sergey-life <sergei.kharchenko888@gmail.com>
2020-08-28 16:38:14 +0300

    test4

--- js/local.js
+++ js/local.js
@@ -1,44 +1,47 @@
 let inputIn = document.querySelector('.form-control');
 let button = document.querySelector('button');
+let local = document.querySelector('.local');
 
-button.onclick = function () {
-    let value = inputIn.value;
-    let input = value;
-    let items = input.split(", ");
+if (local != null) {
+    button.onclick = function () {
+        let value = inputIn.value;
+        let input = value;
+        let items = input.split(", ");
 
-    function permutator(inputArr) {
-        var results = [];
+        function permutator(inputArr) {
+            var results = [];
 
-        function permute(arr, memo) {
-            var cur, memo = memo || [];
+            function permute(arr, memo) {
+                var cur, memo = memo || [];
 
-            for (var i = 0; i < arr.length; i++) {
-                cur = arr.splice(i, 1);
-                if (arr.length === 0) {
-                    results.push(memo.concat(cur));
+                for (var i = 0; i < arr.length; i++) {
+                    cur = arr.splice(i, 1);
+                    if (arr.length === 0) {
+                        results.push(memo.concat(cur));
+                    }
+                    permute(arr.slice(), memo.concat(cur));
+                    arr.splice(i, 0, cur[0]);
                 }
-                permute(arr.slice(), memo.concat(cur));
-                arr.splice(i, 0, cur[0]);
+
+                return results;
             }
 
-            return results;
+            return permute(inputArr);
         }
 
-        return permute(inputArr);
+        const div = document.getElementById('container')
+        const list = permutator(items)
+
+        list.forEach(i => {
+            const el = document.createElement('p')
+
+            el.textContent = i.toString()
+
+            div.appendChild(el)
+            console.log(list)
+        })
     }
-
-    const div = document.getElementById('container')
-    const list = permutator(items)
-
-    list.forEach(i => {
-        const el = document.createElement('p')
-
-        el.textContent = i.toString()
-
-        div.appendChild(el)
-    })
 }
-
 
 
 
--- server.php
+++ server.php
@@ -1,23 +1,21 @@
 <?php
 $result = ($_POST['words']);
-if (isset($_POST['local'])) {
-    exit();
-}else {
-    function pc_permute($items, $perms = [])
-    {
-        if (empty($items)) {
-            print join(', ', $perms) . "<br><p></p>";
-        } else {
-            for ($i = count($items) - 1; $i >= 0; --$i) {
-                $newItems = $items;
-                $newPerms = $perms;
-                list($foo) = array_splice($newItems, $i, 1);
-                array_push($newPerms, $foo);
-                pc_permute($newItems, $newPerms);
-            }
+
+function pc_permute($items, $perms = [])
+{
+    if (empty($items)) {
+        print join(', ', $perms) . "<br><p></p>";
+    } else {
+        for ($i = count($items) - 1; $i >= 0; --$i) {
+            $newItems = $items;
+            $newPerms = $perms;
+            list($foo) = array_splice($newItems, $i, 1);
+            array_push($newPerms, $foo);
+            pc_permute($newItems, $newPerms);
         }
     }
-
-    pc_permute(explode(", ", $result));
 }
+
+pc_permute(explode(", ", $result));
+
 
